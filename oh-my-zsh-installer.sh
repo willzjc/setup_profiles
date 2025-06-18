e07784f15eede6c5cdb1d6a4ae02abddea425d6c
Will Chen <willjc@gmail.com>
2025-06-18 23:23:46 +1000

    fix: oh my zsh setup

--- oh-my-zsh-installer.sh
+++ oh-my-zsh-installer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Copying new theme to oh my zsh themes folder"
-cp ./oh-my-zsh/customtheme.zsh-theme $HOME/.oh-my-zsh/themes/.
+mkdir -p $HOME/.oh-my-zsh/themes
+cp ./settings-for-oh-my-zsh/customtheme.zsh-theme $HOME/.oh-my-zsh/themes/.
 echo "setting ~/.zshrc theme"
-sed -i '' 's/^ZSH_THEME=.*/ZSH_THEME="customtheme"/' ~/.zshrc
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="customtheme"/' ~/.zshrc
